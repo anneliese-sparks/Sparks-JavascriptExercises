73a86927bffce4f941c12c8eff270315df3a668a
Anneliese Sparks <sparksanneliese@gmail.com>
2018-06-21 11:14:01 -0400

    four exercises updated and added

--- script.js
+++ script.js
@@ -1,3 +1,53 @@
+var total = 0;
+
+for (var num = 1; <= 5; num++) {
+
+  total = total + num;
+
+  console.log (total)
+
+}
+
+
+
+var answer = prompt('Do you want to play?');
+
+if ( answer === 'yes' ) {
+
+  prompt("Enter a word");
+
+  prompt("Do you want to play again?");
+
+} else { 
+
+  }
+
+
+var answer = prompt('Would you like to print your name - Yes or No?');
+
+if ( answer === 'yes' ) {
+
+  prompt("Please enter your name");
+
+  prompt("Would you like to print your name again?");
+
+if ( answer === 'yes' ) {
+
+}
+  prompt("Please enter your name");
+
+  } else { 
+
+  }
+
+
+
+
+
+var select = document.querySelector('select');
+var para = document.querySelector('p');
+
+select.addEventListener('change', setMeal);
 
 function setMeal() {
   var choice = select.value;
@@ -5,7 +55,7 @@ function setMeal() {
   if (choice === 'morning') {
     para.textContent = 'Since it is morning, we suggest eggs and toast';
   } else if (choice === 'noon') {
-    para.textContent = 'Since it's the afternoon, we suggest a salad;
+    para.textContent = 'Since it is the afternoon, we suggest a salad;
   } else if (choice === 'evening') {
     para.textContent = 'Since it is the evening, we suggest chicken and rice';
   } else {
